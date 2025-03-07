a89a28e86fee3627ed09e3d4114a264000546d3b
Nikhil garg <nikhilgarg.drb@gmail.com>
2025-03-08 00:02:58 +0530

    Database connect

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+
 const app = express();
+dotenv.config();
 
-
-app.use(express.json());
-
-
-
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+port =process.env.PORT;
+connectDB().then(function () {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch(function (error) {
+    console.log(error);
 });
+
